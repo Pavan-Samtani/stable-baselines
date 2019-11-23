2fd84e09b57c2b3430cd06b656c2a6e640d907bb
pavan-samtani <pavan3398@gmail.com>
2019-11-23 02:00:51 -0300

    Changes to PPO,
    Multi agent, centralized actor and critic

--- stable_baselines/ppo2/ppo2.py
+++ stable_baselines/ppo2/ppo2.py
@@ -464,23 +464,32 @@ class Runner(AbstractEnvRunner):
         mb_obs, mb_rewards, mb_actions, mb_values, mb_dones, mb_neglogpacs = [], [], [], [], [], []
         mb_states = self.states
         ep_infos = []
+        num_players = self.env.num_players
         for _ in range(self.n_steps):
-            actions, values, self.states, neglogpacs = self.model.step(self.obs, self.states, self.dones)
-            mb_obs.append(self.obs.copy())
-            mb_actions.append(actions)
-            mb_values.append(values)
-            mb_neglogpacs.append(neglogpacs)
-            mb_dones.append(self.dones)
+        	if isinstance(self.states, list):
+        		self.states = [self.states for _ in range(num_players)]
+        	all_actions = []
+        	for i in range(num_players):
+        		actions, values, self.states[i], neglogpacs = self.model.step(self.obs[i], self.states[i], self.dones)
+	            mb_obs.append(self.obs[i].copy())
+	            mb_actions.append(actions)
+	            mb_values.append(values)
+	            mb_neglogpacs.append(neglogpacs)
+	            mb_dones.append(self.dones)
+	            all_actions.append(np.squeeze(actions))
+	        actions = all_actions
             clipped_actions = actions
             # Clip the actions to avoid out of bound error
             if isinstance(self.env.action_space, gym.spaces.Box):
-                clipped_actions = np.clip(actions, self.env.action_space.low, self.env.action_space.high)
+            	for i in range(num_players):
+            		clipped_actions[i] = np.clip(actions[i], self.env.action_space.low, self.env.action_space.high)
             self.obs[:], rewards, self.dones, infos = self.env.step(clipped_actions)
             for info in infos:
                 maybe_ep_info = info.get('episode')
                 if maybe_ep_info is not None:
                     ep_infos.append(maybe_ep_info)
-            mb_rewards.append(rewards)
+            for x in rewards:
+            	mb_rewards.append(x)
         # batch of steps to batch of rollouts
         mb_obs = np.asarray(mb_obs, dtype=self.obs.dtype)
         mb_rewards = np.asarray(mb_rewards, dtype=np.float32)
@@ -488,20 +497,23 @@ class Runner(AbstractEnvRunner):
         mb_values = np.asarray(mb_values, dtype=np.float32)
         mb_neglogpacs = np.asarray(mb_neglogpacs, dtype=np.float32)
         mb_dones = np.asarray(mb_dones, dtype=np.bool)
-        last_values = self.model.value(self.obs, self.states, self.dones)
+        last_values = [self.model.value(self.obs[i], self.states[i], self.dones) for i in range(num_players)]
         # discount/bootstrap off value fn
         mb_advs = np.zeros_like(mb_rewards)
         true_reward = np.copy(mb_rewards)
-        last_gae_lam = 0
+        last_gae_lam = [0 for _ in range(num_players)]
+ 		nextvalues = [0 for _ in range(num_players)]
         for step in reversed(range(self.n_steps)):
-            if step == self.n_steps - 1:
-                nextnonterminal = 1.0 - self.dones
-                nextvalues = last_values
-            else:
-                nextnonterminal = 1.0 - mb_dones[step + 1]
-                nextvalues = mb_values[step + 1]
-            delta = mb_rewards[step] + self.gamma * nextvalues * nextnonterminal - mb_values[step]
-            mb_advs[step] = last_gae_lam = delta + self.gamma * self.lam * nextnonterminal * last_gae_lam
+        	for player in range(num_players):
+        		idx = step * num_players + player
+	            if step == self.n_steps - 1:
+	                nextnonterminal = 1.0 - self.dones
+	                nextvalues[player] = last_values[idx]
+	            else:
+	                nextnonterminal = 1.0 - mb_dones[idx + num_players]
+	                nextvalues[player] = mb_values[idx + num_players]
+	            delta = mb_rewards[idx] + self.gamma * nextvalues[player] * nextnonterminal - mb_values[idx]
+	            mb_advs[idx] = last_gae_lam[player] = delta + self.gamma * self.lam * nextnonterminal * last_gae_lam[player]
         mb_returns = mb_advs + mb_values
 
         mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs, true_reward = \
